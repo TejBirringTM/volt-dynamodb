7b67b46f54f10d9414bc9fd9ad87d35e254b9b7b
Tej Birring <tejbirring@gmail.com>
2026-01-25 03:21:55 +0000

    build: ensure bundler (tsup) uses tsconfig.build.json as TS config for transpilation

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  config: 'tsconfig.build.json',
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
