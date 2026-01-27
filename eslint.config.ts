b4319e6644720a2c735d7bff6ad438242f742603
Tej Birring <tejbirring@gmail.com>
2026-01-27 01:16:26 +0000

    revert: disable use of console.* for logging (use ESLint instead of interface redeclaration)

--- eslint.config.ts
+++ eslint.config.ts
@@ -19,6 +19,7 @@ export default tseslint.config(
       },
     },
     rules: {
+      'no-console': ['error'],
       '@typescript-eslint/explicit-function-return-type': 'warn',
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
